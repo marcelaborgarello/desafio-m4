ac5565441e805c6582cba0e0dda3ea7cbbf1458b
imprenart <imprenart@gmail.com>
2022-06-22 10:50:02 -0300

    commit 5

--- components/header/header.js
+++ components/header/header.js
@@ -7,9 +7,9 @@ const headerComponent = (el) => {
 
       <nav class="header-component-nav">
         <ul>
-          <li><a href="../../portfolio.html">Portfolio</a></li>
-          <li><a href="../../services.html">Servicios</a></li>
-          <li><a href="../../contact.html">Contacto</a></li>
+          <li><a href="./portfolio.html">Portfolio</a></li>
+          <li><a href="./services.html">Servicios</a></li>
+          <li><a href="./contact.html">Contacto</a></li>
         </ul>
       </nav>
       <div class="menu">
